e990f99c1ba776de3399e4b25db5112621c8ca01
barokool <knabao7a7@gmail.com>
2022-12-05 23:00:40 +0700

    asdasdasd

---
+++ src/app.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  uploadImage(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
+
+  @Get('/:imageName')
+  getImage(@Param('imageName') imageName, @Res() res) {
+    res.sendFile(join(process.cwd(), 'uploads/' + imageName));
+  }
+}
